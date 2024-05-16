d4c131381f3fd9fb47e12e8d04868a0a22a9ed19
srinath sriramula <sriramulasrinath59@gmail.com>
2024-05-16 10:33:14 +0530

    new changes

---
+++ variables/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "db" {
+
+    ami = var.image_id
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+    instance_type = var.instance_type
+    tags = var.tags
+}
+
+
+resource "aws_security_group" "allow-ssh" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  ingress {
+    from_port        = var.ssh_protocol
+    to_port          = var.ssh_protocol
+    protocol         = var.protocol
+    cidr_blocks      = var.allowing-cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" #for all protocols like tcp http..
+    cidr_blocks      = var.allowing-cidr
+  }
+
+  tags = {
+    name = "allow-ssh"
+    createdBy = "Srinath"
+  }
+}
---
+++ variables/variable.tf
@@ -0,0 +1,39 @@
+variable "image_id" {
+    type = string
+    default = "ami-090252cbe067a9e58"
+    description = "RHEL AMI id"
+}
+
+variable "instance_type" {
+  default = "t2.micro" 
+}
+
+variable "tags" {
+    default = {
+        name = "DB"
+        Module = "DB"
+        Project = "Expense"
+        Environment = "Dev"
+    }
+}
+
+variable "sg_name" {
+    default = "ssh_allow"
+}
+
+variable "sg_description" {
+    default = "allowing-ssh-access"
+}
+
+variable "ssh_protocol" {
+    default = 22
+}
+
+variable "protocol" {
+  default = "tcp"
+}
+
+variable "allowing-cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
