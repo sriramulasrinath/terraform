3077a83b690ac1c9b6800fd21227e9bfa2ec0f29
srinath sriramula <sriramulasrinath59@gmail.com>
2024-05-19 12:11:59 +0530

    commiting changes

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,10 +1,20 @@
 data "aws_ami" "ami_id" {
+
   most_recent = true
-  owners = [""]
+  owners = ["973714476881"]
   filter {
     name   = "name"
-    values = ["RHEL-9-Devops-Practice"]
+    values = ["RHEL-9-DevOps-Practice"]
   }
+  filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
 
 data "aws_vpc" "default" {
--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "expense" {
+resource "aws_instance" "db" {
     ami = data.aws_ami.ami_id.id
-    vpc_security_group_ids =  "sg-0dc8ba8e38b8d1861"
+    vpc_security_group_ids = ["sg-00d8e884e38dae954"]
     instance_type = "t2.micro"
     tags = {
         Name = "data-source-practice "
